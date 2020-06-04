9f33d20d3004d4d831695f4517585b4378d9874d
Yevhenii Spivak <Yevhenii.spivak@gmail.com>
2020-06-04 15:50:19 +0300

    new

---
+++ lesson32_step8_assert.py
@@ -0,0 +1,10 @@
+    # решение через + (не забываем ставить правильно пробелы)
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, "expected " + expected_result + ", got " + actual_result
+    # решение через .format
+def test_input_text2(expected_result, actual_result):
+
+    assert expected_result == actual_result, "expected {}, got {}".format(expected_result, actual_result)
+    # Решение через f'
+def test_input_text3(expected_result, actual_result):
+    assert expected_result == actual_result, f'expected {expected_result}, got {actual_result}'
