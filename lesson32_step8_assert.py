688ea7f1390a7d9bf71d21b4f05315e1fc44de15
Yevhenii Spivak <Yevhenii.spivak@gmail.com>
2020-06-04 16:08:58 +0300

    new

--- lesson32_step8_assert.py
+++ lesson32_step8_assert.py
@@ -1,10 +1,59 @@
-    # решение через + (не забываем ставить правильно пробелы)
-def test_input_text(expected_result, actual_result):
-    assert expected_result == actual_result, "expected " + expected_result + ", got " + actual_result
-    # решение через .format
-def test_input_text2(expected_result, actual_result):
+import unittest
+from selenium import webdriver
+import time
 
-    assert expected_result == actual_result, "expected {}, got {}".format(expected_result, actual_result)
-    # Решение через f'
-def test_input_text3(expected_result, actual_result):
-    assert expected_result == actual_result, f'expected {expected_result}, got {actual_result}'
+
+class TestAbs(unittest.TestCase):
+    link1 = "http://suninjuly.github.io/registration1.html"
+    link2 = "http://suninjuly.github.io/registration2.html"
+
+    def test_lesson16_step10_assert(self):
+        browser = webdriver.Chrome()
+        browser.get(self.link1)
+
+        input1 = browser.find_element_by_css_selector("[placeholder='Input your first name']").send_keys("Hello")
+        input2 = browser.find_element_by_css_selector("[placeholder='Input your last name']").send_keys("Hello")
+        input3 = browser.find_element_by_css_selector("[placeholder='Input your email']").send_keys("Hello")
+        button = browser.find_element_by_css_selector("button.btn").click()
+        time.sleep(3)
+
+        # Находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
+
+    def test_lesson16_step10_assert_false(self):
+        browser = webdriver.Chrome()
+        browser.get(self.link2)
+
+        input1 = browser.find_element_by_css_selector("[placeholder='Input your first name']").send_keys("Hello")
+        input2 = browser.find_element_by_css_selector("[placeholder='Input your last name']").send_keys("Hello")
+        input3 = browser.find_element_by_css_selector("[placeholder='Input your email']").send_keys("Hello")
+        button = browser.find_element_by_css_selector("button.btn").click()
+        time.sleep(3)
+
+        # Находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+#     # решение через + (не забываем ставить правильно пробелы)
+# def test_input_text(expected_result, actual_result):
+#     assert expected_result == actual_result, "expected " + expected_result + ", got " + actual_result
+#     # решение через .format
+# def test_input_text2(expected_result, actual_result):
+#
+#     assert expected_result == actual_result, "expected {}, got {}".format(expected_result, actual_result)
+#     # Решение через f'
+# def test_input_text3(expected_result, actual_result):
+#     assert expected_result == actual_result, f'expected {expected_result}, got {actual_result}'
